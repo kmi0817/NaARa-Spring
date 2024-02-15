c95263ae884052ddf34057984daec27f3e46cff4
kmi0817 <kmi0817@naver.com>
2024-02-15 22:41:58 +0900

    feat: Spring Initializr로 기본 환경 구성

---
+++ src/main/java/kmi0817/naara/NaARaApplication.java
@@ -0,0 +1,13 @@
+package kmi0817.naara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaARaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaARaApplication.class, args);
+	}
+
+}
